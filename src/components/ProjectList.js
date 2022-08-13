2e2210eac976c4d5f07d70143df934b1f889874a
Daniel Obare <danielobare551@gmail.com>
2022-08-13 16:02:50 +0300

    COMPLETED

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,18 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map((projectObj) => {
+          return (
+            <ProjectItem
+              key={projectObj.id}
+              name={projectObj.name}
+              about={projectObj.about}
+              technologies={projectObj.technologies}
+            />)
+            }
+          ) 
+        }
+      </div>
     </div>
   );
 }
